7ebef4c40261d3aecad69e67afb74ab45c889d77
Four_ST <149815650+FourMinato@users.noreply.github.com>
2026-03-11 21:16:02 +0700

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'https://TEST-PRODUCTION-API.com', // waiting for domain
-    googleClientId: '619409196840-amjc361lf1s18kb80ov215cjflsieml9.apps.googleusercontent.com',
-     reCaptchaSitekey: '6Lc8GiMsAAAAAHQ3cJWXwUXQvLXKZe0ft3Yx6aTa'
-  };
